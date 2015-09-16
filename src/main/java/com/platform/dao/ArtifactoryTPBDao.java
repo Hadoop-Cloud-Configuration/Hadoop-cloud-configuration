2cd033c776b1cffd4522852e9c55714fbd4a55d2
maxlpy <maxlipengyu@gmail.com>
2015-09-15 21:02:26 -0400

    first commit

---
+++ src/main/java/com/platform/dao/ArtifactoryTPBDao.java
@@ -0,0 +1,12 @@
+//package com.platform.dao;
+//import java.util.List;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.platform.bean.ArtifactoryTPB;
+//
+//@Repository
+//public interface ArtifactoryTPBDao extends CrudRepository<ArtifactoryTPB, Long> {
+//	List<ArtifactoryTPB> findAll();
+//}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+
+<body>
+	<h1>Welcome to use Spring Boot.</h1>
+	<h3>Some useful Links:</h3>  
+	
+    <a href="http://projects.spring.io/spring-boot/" target="_blank">Spring Boot</a><br>
+</body>
+</html>
