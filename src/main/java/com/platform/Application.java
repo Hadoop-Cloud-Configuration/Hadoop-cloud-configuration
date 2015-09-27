26f898171bdd705d57915d9ffc47bccd378b2a9c
maxlpy <maxlipengyu@gmail.com>
2015-09-27 14:02:53 -0400

    add ambari jar

--- src/main/java/com/platform/Application.java
+++ src/main/java/com/platform/Application.java
@@ -4,9 +4,12 @@ package com.platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sequenceiq.ambari.client.AmbariClient;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+    	AmbariClient test = new AmbariClient(); 
         SpringApplication.run(Application.class, args);
     }
 }
